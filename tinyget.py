2755aebc2663102a733fab0814262119647a2a46
martinval11 <100082171+martinval11@users.noreply.github.com>
2022-03-08 21:41:43 -0300

    Add files via upload

--- tinyget.py
+++ tinyget.py
@@ -3,30 +3,49 @@ import requests
 import shutil
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-g','--get', type=str, help="HTTP GET Request", required=False)
-parser.add_argument('-o','--output', type=str, help="Output of the request", required=True)
-parser.add_argument('-p','--post', help="HTTP POST Request", required=False)
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g","--get", type=str, help="HTTP GET Request", required=False)
+    parser.add_argument("-o","--output", type=str, help="Output of the request", required=True)
+    parser.add_argument("-p","--post", help="HTTP POST Request", required=False)
+    args = parser.parse_args()
 
-if args.post:
-    res_post = requests.post(args.post, stream=True)
+    # Color Aliases
+    RESET = Fore.RESET
+    RED   = Fore.RED
+    GREEN = Fore.GREEN
 
-    if res_post:
-        output_post = args.output
-        with open(output_post, 'wb') as f:
-            res_post.raw.decode_content = True
-            shutil.copyfileobj(res_post.raw, f)
-    else:
-        print("Response Failed" + Fore.RED, "(", res_post.status_code, ")" + Fore.RESET)
+    if args.post:
+        print("Resolving URL...")
+        try:
+            res_post = requests.post(args.post, stream=True)
 
-if args.get:
-    res = requests.get(args.get, stream=True)
+            if res_post:
+                output_post = args.output
+                with open(output_post, "wb") as f:
+                    res_post.raw.decode_content = True
+                    shutil.copyfileobj(res_post.raw, f)
+                print(GREEN + "The request has been successful!" + RESET)
+            else:
+                print("Response Failed" + RED, "(", res_post.status_code, ")" + RESET)
 
-    if res:
-        output = args.output
-        with open(output, 'wb') as f:
-            res.raw.decode_content = True
-            shutil.copyfileobj(res.raw, f)
-    else:
-        print("Response Failed" + Fore.RED, "(", res.status_code, ")" + Fore.RESET)
+        except requests.exceptions.ConnectionError:
+            print(RED + "Failed to establish a new connection (Temporary failure in name resolution)" + RESET)
+
+    if args.get:
+        try:
+            print("Resolving URL...")
+            res = requests.get(args.get, stream=True)
+            
+            if res:
+                output = args.output
+                with open(output, "wb") as f:
+                    res.raw.decode_content = True
+                    shutil.copyfileobj(res.raw, f)
+                print(GREEN + "The request has been successful!" + RESET)
+            else:
+                print("Response Failed" + RED, "(", res.status_code, ")" + RESET)
+
+        except requests.exceptions.ConnectionError:
+            print(RED + "Failed to establish a new connection (Temporary failure in name resolution)" + RESET)
+main()
